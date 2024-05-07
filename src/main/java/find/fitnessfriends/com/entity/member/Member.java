b78b7372780caf19e52a85d8fcdcc6ab8cfc996e
jongchan <jongchan1009@naver.com>
2024-05-07 15:26:33 +0900

    feat: member 객체 생성

---
+++ src/main/java/find/fitnessfriends/com/entity/member/Member.java
@@ -0,0 +1,30 @@
+package find.fitnessfriends.com.entity.member;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "logoinId", length = 20)
+    private String loginId;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "nickname", length = 20)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @CreationTimestamp
+    private Timestamp createdDate;
+
+}
