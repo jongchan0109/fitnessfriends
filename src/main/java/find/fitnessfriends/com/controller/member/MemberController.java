5da69c30291e5dcca5c82d9b84b451fa2592609a
jongchan <jongchan1009@naver.com>
2024-05-07 16:24:17 +0900

    feat: 회원가입 성공시 세션 생성 구현

--- src/main/java/find/fitnessfriends/com/controller/member/MemberController.java
+++ src/main/java/find/fitnessfriends/com/controller/member/MemberController.java
@@ -3,6 +3,8 @@ package find.fitnessfriends.com.controller.member;
 import find.fitnessfriends.com.dto.member.JoinDto;
 import find.fitnessfriends.com.entity.member.Member;
 import find.fitnessfriends.com.service.member.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -25,13 +27,16 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String join(@Validated @ModelAttribute JoinDto joinDto, BindingResult bindingResult) {
+    public String join(@Validated @ModelAttribute JoinDto joinDto, BindingResult bindingResult, HttpServletRequest request) {
 
         if (bindingResult.hasErrors()) {
             return "member/joinForm";
         }
 
-        memberService.joinProcess(joinDto.getLoginId(),joinDto.getPassword(), joinDto.getNickname());
+        Member loginMember = memberService.joinProcess(joinDto.getLoginId(), joinDto.getPassword(), joinDto.getNickname());
+
+        HttpSession session = request.getSession();
+        session.setAttribute("loginMember", loginMember);
 
         return "redirect:/";
     }
