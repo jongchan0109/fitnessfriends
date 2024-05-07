c8c7eb8962213d1767b1b33e0c64492c3af4a2a4
jongchan <jongchan1009@naver.com>
2024-05-07 15:36:46 +0900

    feat: 회원가입 화면 이동 함수 생성

---
+++ src/main/java/find/fitnessfriends/com/controller/member/MemberController.java
@@ -0,0 +1,15 @@
+package find.fitnessfriends.com.controller.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "member/joinForm";
+    }
+}
