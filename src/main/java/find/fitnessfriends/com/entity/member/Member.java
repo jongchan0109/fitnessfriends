93194796d64936049eb12c0c3623a98cafbd66d1
jongchan <jongchan1009@naver.com>
2024-05-07 15:51:21 +0900

    feat: Member 생성자 추가

--- src/main/java/find/fitnessfriends/com/entity/member/Member.java
+++ src/main/java/find/fitnessfriends/com/entity/member/Member.java
@@ -27,4 +27,11 @@ public class Member {
     @CreationTimestamp
     private Timestamp createdDate;
 
+
+    public Member(String loginId, String password, String nickname) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = Role.MEMBER;
+    }
 }
