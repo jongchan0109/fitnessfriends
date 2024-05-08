35d5ed3475d9d6300577d3eaa3b683a13b58ec52
jongchan <jongchan1009@naver.com>
2024-05-08 16:37:29 +0900

    feat: 게시글 작성 기능 추가

--- src/main/java/find/fitnessfriends/com/controller/board/BoardController.java
+++ src/main/java/find/fitnessfriends/com/controller/board/BoardController.java
@@ -1,13 +1,21 @@
 package find.fitnessfriends.com.controller.board;
 
+import find.fitnessfriends.com.dto.board.WriteDto;
+import find.fitnessfriends.com.entity.board.Board;
+import find.fitnessfriends.com.entity.member.Member;
 import find.fitnessfriends.com.service.board.BoardService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,6 +34,21 @@ public class BoardController {
     @GetMapping("/writeForm")
     public String writeForm() {
         return "board/writeForm";
+    }
+
+    @PostMapping("/write")
+    public String write(@ModelAttribute WriteDto writeDto, BindingResult bindingResult, HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "board/writeForm";
+        }
+
+        HttpSession session = request.getSession();
+        Member member = (Member)session.getAttribute("loginMember");
+
+        Board board = boardService.write(writeDto.getTitle(), writeDto.getContent(), member);
+
+        return "redirect:/board";
     }
 
 
