11b115ac87107ea17f4549def3cd15cce47f03db
jongchan <jongchan1009@naver.com>
2024-05-09 16:19:15 +0900

    feat: updateDto 생성

---
+++ src/main/java/find/fitnessfriends/com/dto/board/UpdateDto.java
@@ -0,0 +1,16 @@
+package find.fitnessfriends.com.dto.board;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateDto {
+
+    @NotBlank(message = "제목을 입력해주세요")
+    private String title;
+
+    @NotBlank(message = "내용을 입력해주세요")
+    private String content;
+}
