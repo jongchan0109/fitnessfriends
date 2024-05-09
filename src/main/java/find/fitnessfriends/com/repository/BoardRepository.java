391d94c0c3b08439711dbe72dcb2914726449825
jongchan <jongchan1009@naver.com>
2024-05-09 17:24:42 +0900

    feat: 인기 게시글 찾기 기능 추가

--- src/main/java/find/fitnessfriends/com/repository/BoardRepository.java
+++ src/main/java/find/fitnessfriends/com/repository/BoardRepository.java
@@ -3,5 +3,8 @@ package find.fitnessfriends.com.repository;
 import find.fitnessfriends.com.entity.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findTop6ByOrderByViewDesc();
 }
