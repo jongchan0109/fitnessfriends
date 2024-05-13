9c90c302a1ccac40305b763825c990c36d9caaa0
jongchan <jongchan1009@naver.com>
2024-05-13 16:04:51 +0900

    feat: 댓글작성 기능 구현

---
+++ src/main/java/find/fitnessfriends/com/controller/board/ReplyController.java
@@ -0,0 +1,48 @@
+package find.fitnessfriends.com.controller.board;
+
+import find.fitnessfriends.com.dto.board.reply.ReplyDto;
+import find.fitnessfriends.com.entity.board.Board;
+import find.fitnessfriends.com.entity.member.Member;
+import find.fitnessfriends.com.service.board.BoardService;
+import find.fitnessfriends.com.service.board.reply.ReplyService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class ReplyController {
+
+    private final BoardService boardService;
+    private final ReplyService replyService;
+
+    @PostMapping("/{id}/reply")
+    public String reply(@PathVariable Long id, @Validated @ModelAttribute ReplyDto replyDto, BindingResult bindingResult, Model model, HttpServletRequest request) {
+
+        Board board = boardService.findById(id);
+        model.addAttribute("board", board);
+        HttpSession session = request.getSession();
+        Member loginMember = (Member)session.getAttribute("loginMember");
+
+        model.addAttribute("loginMember", loginMember);
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("replyDto = " + replyDto.getContent());
+            return "redirect:/board/detail/" + board.getId();
+        }
+
+        replyService.writeProcess(board.getId(),loginMember.getId(), replyDto.getContent());
+
+        return "redirect:/board/detail/" + board.getId();
+    }
+
+}
