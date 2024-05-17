766ca8104a7cfc3a4ce8c27b10cf97f5901f9d30
jongchan <jongchan1009@naver.com>
2024-05-17 15:11:47 +0900

    feat: 매칭 할 수 있는 멤버 조회 기능 추가

--- src/main/java/find/fitnessfriends/com/repository/MemberRepository.java
+++ src/main/java/find/fitnessfriends/com/repository/MemberRepository.java
@@ -1,9 +1,17 @@
 package find.fitnessfriends.com.repository;
 
 import find.fitnessfriends.com.entity.member.Member;
+import find.fitnessfriends.com.entity.member.matching.Ability;
+import find.fitnessfriends.com.entity.member.matching.Gender;
+import find.fitnessfriends.com.entity.member.matching.PreferredTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
+
+    List<Member> findByOppMatchingInfo_AbilityAndOppMatchingInfo_GenderAndOppMatchingInfo_PreferredTime(
+            Ability ability, Gender gender, PreferredTime preferredTime);
 }
